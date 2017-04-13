4628f32b4d5dd5f7404cda615586668c78382f5a
Pietro Rignanese <pietro_rignanese@hotmail.it>
2017-04-13 14:47:55 +0200

    Update Nodo_2 - Incompleto.ino

--- Sketch/Sketch Definitivi/Nodo_2 - Incompleto.ino
+++ Sketch/Sketch Definitivi/Nodo_2 - Incompleto.ino
@@ -2,6 +2,10 @@
 
 //-------------------------------------------------------PIETRO RIGNANESE & ANDREA POLENTA---------------------------------
 
+//Librerie per gestione display
+#include <Wire.h> 
+#include <LiquidCrystal_I2C.h>
+
 //Libreria IR
 #include <IRremote.h>
 int RECV_IR = 8;
@@ -12,8 +16,16 @@ decode_results results;
 int relay = 2;
 int attuatore = 4;
 
+LiquidCrystal_I2C lcd(0x27,20,4);
+
 void setup() 
 {
+  //Display
+  lcd.init();                     
+  lcd.backlight();
+  lcd.home();
+  lcd.print("Nodo 2");
+  
   Serial.begin(115200);
   Serial.println("Nodo 2");
   Serial.println("");
@@ -32,14 +44,21 @@ void setup()
 
 void loop() 
 {
+  lcd.setCursor(0,0);
+  lcd.print("Nodo 2          ");
+  lcd.print("                ");
   
   //Controlla se è stato ricevuto un dato sul sensore IR
   if (irrecv.decode(&results)) 
   {
+    lcd.setCursor(0,0);
+    lcd.print("Comunicazione:");
+    lcd.setCursor(0,1);
     Serial.println(results.value);
      //Accensione Relay nodo 2
     if(results.value == 69632)
     {
+      lcd.print("Relay ON  ");
       Serial.println("Accensione Realy");
       digitalWrite(relay,HIGH);
     }
@@ -47,6 +66,7 @@ void loop()
     //Accensione Attuatore nodo 2
     if(results.value == 17895424)
     {
+      lcd.print("Attua. ON ");
       Serial.println("Accensione Attuatore");
       digitalWrite(attuatore, HIGH);
     }
@@ -54,6 +74,7 @@ void loop()
     //Spegnimento Relay nodo 2
     if(results.value == 65536)
     {
+      lcd.print("Relay OFF ");
       Serial.println("Spegnimento Realy");
       digitalWrite(relay, LOW);
     }
@@ -61,6 +82,7 @@ void loop()
     //Spegnimento Attuatore nodo 2
     if(results.value == 286261248)
     {
+      lcd.print("Attua. OFF");
       Serial.println("Spegnimento Attuatore");
       digitalWrite(attuatore, LOW);
     }
